6e4df53af53ad1245ab979a20abcbd67a0de7ee1
Juan David Calderon <jd.calderong12@uniandes.edu.co>
2025-04-06 10:17:18 -0500

    taller 2

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,24 @@ var promedioTemporadasElm = document.getElementById("temporadas-promedio");
 renderSeriesInTable(series);
 promedioTemporadasElm.innerHTML = "".concat(getTemporadasPromedio(series));
 function renderSeriesInTable(se) {
+    var cardContainer = document.getElementById("cardContainer");
     se.forEach(function (s) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(s.codigo, "</td>\n                           <td>").concat(s.nombre, "</td>\n                           <td>").concat(s.canal, "</td>\n                           <td>").concat(s.temporadas, "</td>");
+        trElement.innerHTML = "<td>".concat(s.codigo, "</td>\n                           <td><a href=\"#\" class=\"serienombrelink\" data-nombre=\"").concat(s.nombre, "\" data-nombre=\"").concat(s.nombre, "\"\n                           data-imagen=\"").concat(s.imagen, "\"\n                           data-descripcion=\"").concat(s.descripcion, "\"\n                           data-link=\"").concat(s.link, "\">").concat(s.nombre, "</a></td>\n                           <td>").concat(s.canal, "</td>\n                           <td>").concat(s.temporadas, "</td>");
         seriesTbody.appendChild(trElement);
+    });
+    var linknombre = document.querySelectorAll(".serienombrelink");
+    linknombre.forEach(function (Serielink) {
+        Serielink.addEventListener("click", function (event) {
+            event.preventDefault();
+            var nombre = Serielink.dataset.nombre;
+            var imagen = Serielink.dataset.imagen;
+            var descripcion = Serielink.dataset.descripcion;
+            var link = Serielink.dataset.link;
+            if (cardContainer) {
+                cardContainer.innerHTML = "\n          <div class=\"card\" style=\"width: 70%;\">\n            <img src=\"".concat(imagen, "\" class=\"card-img-top\" alt=\"Imagen de la serie ").concat(nombre, " con link ").concat(imagen, "\">\n            <div class=\"card-body\">\n              <h5 class=\"card-nombre\">").concat(nombre, "</h5>\n              <p class=\"card-descripcion\">").concat(descripcion, "</p>\n              <a href=\"").concat(link, "\" class=\"boton\">").concat(link, "</a>\n    </div>\n  </div>\n        ");
+            }
+        });
     });
 }
 function getTemporadasPromedio(series) {
